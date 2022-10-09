f003cbf3905b74ba40ce8300bc29fbf38047e2dc
JoaoPauloHenriqueSiqueira <joao.henriquesiqueirati@gmail.com>
2022-10-09 11:41:40 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:search_gis/ui/home_page.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+      theme: ThemeData(
+          hintColor: Colors.white,
+          inputDecorationTheme: InputDecorationTheme(
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white))))
+  ));
+}
---
+++ lib/ui/gif_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_share/flutter_share.dart';
+
+class GifPage extends StatelessWidget {
+  final Map _gifData;
+
+  GifPage(this._gifData);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_gifData["title"]),
+        backgroundColor: Colors.black,
+        actions: <Widget>[
+          IconButton(
+              onPressed: () {
+                print("a");
+                FlutterShare.share(
+                    title: _gifData["title"],
+                    linkUrl: _gifData["images"]["fixed_height"]["url"]);
+              },
+              icon: Icon(Icons.share))
+        ],
+      ),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Image.network(_gifData["images"]["fixed_height"]["url"]),
+      ),
+    );
+  }
+}
